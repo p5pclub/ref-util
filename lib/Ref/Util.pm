fc53540549b13d75376bd64867a2a654a0dcaa07
Sawyer X <xsawyerx@cpan.org>
2015-12-28 20:54:19 +0100

    bump version, release

--- lib/Ref/Util.pm
+++ lib/Ref/Util.pm
@@ -6,7 +6,7 @@ use XSLoader;
 
 use Exporter 5.57 'import';
 
-our $VERSION     = '0.004';
+our $VERSION     = '0.005';
 our %EXPORT_TAGS = ( 'all' => [qw<
     is_scalarref is_arrayref is_hashref is_coderef is_regexpref
     is_globref is_formatref is_ioref is_refref
