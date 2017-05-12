9ef23523c1a3e62a82089be0bea09b6baff966b6
Karen Etheridge <ether@cpan.org>
2017-05-12 15:01:50 +0200

    remove misuse of DynamicPrereqs for unrelated makefile snippet

--- makefile-pl-inc.pl
+++ makefile-pl-inc.pl
@@ -1,8 +1,3 @@
-if (!want_pp() && can_xs()) {
-    test_requires('Ref::Util::XS');
-    runtime_requires('Ref::Util::XS');
-}
-
 if (eval { require Ref::Util } && Ref::Util->VERSION < 0.114) {
   package MY;
   no warnings 'once';
